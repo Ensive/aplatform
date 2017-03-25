ee5ff3ec8e5fc0f01fbf392181cce8785fdf9c9d
Anton Goncharuk <antonge92@gmail.com>
2017-03-25 21:19:14 +0200

    Add "blog" app to INSTALLED_APPS. (settings) Comment out CsrfViewMiddleware. Add '/posts' url matching

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 
 
 def index(request):
-    if request.method == 'GET':
-        print request.__dict__
+    if request.method == "GET":
+        # print request.__dict__
         return JsonResponse({"message": "Hello, world. You're at the aplatform index."})
+    elif request.method == "POST":
+        return JsonResponse({"message": "Post was created."})
